54c4a6e76eba7aeeba9d23807d33f1030ff3f8c7
pandillapalli venkata manoj kumar <venkatamanojkumar0510@gmail.com>
2025-08-19 18:52:21 +0530

    create S3 terraform

---
+++ AWS-S3/main.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket         = var.bucket_name
+  acl         = "private"
+
+  tags = {
+    Name = "manoj"
+  }
+}
+    
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.bucket.id 
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_object" "file" {
+  bucket = aws_s3_bucket.bucket.id
+  key = "notes"
+  source = "readme"
+  acl = "private"
+  etag = filemd5("readme")
+}
---
+++ AWS-S3/variables.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+    type = string
+    default = "manoj8008bucket"
+  
+}
+variable "aws_region" {
+    type = string
+    default = "us-west-2"
+  
+}
