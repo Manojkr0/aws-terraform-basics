c3066c163f72e813367d2addd44dcdadd84331e7
pandillapalli venkata manoj kumar <venkatamanojkumar0510@gmail.com>
2025-08-19 19:12:35 +0530

    Region pipeline fix

--- AWS-S3/variables.tf
+++ AWS-S3/variables.tf
@@ -1,10 +1,7 @@
 variable "bucket_name" {
     type = string
-    default = "manoj8008bucket"
   
 }
 variable "aws_region" {
-    type = string
-    default = "us-west-2"
-  
+    type = string  
 }
