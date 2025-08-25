60d0e72ed0724d97501ae4551ae4d916f4e93256
pandillapalli venkata manoj kumar <venkatamanojkumar0510@gmail.com>
2025-08-25 12:36:18 +0530

    from default vpc to custom one

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,6 +1,8 @@
 resource "aws_instance""ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  subnet_id = var.subnet_id
+  security_groups = [var.security_group]
 
   tags = {
     Name = "MyEC2Instance"
