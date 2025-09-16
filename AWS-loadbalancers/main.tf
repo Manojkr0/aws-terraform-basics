3048f7b01ac1e026170e3e53fa79ff3893411a0c
pandillapalli venkata manoj kumar <venkatamanojkumar0510@gmail.com>
2025-09-16 17:50:04 +0530

    fixes

--- AWS-loadbalancers/main.tf
+++ AWS-loadbalancers/main.tf
@@ -85,11 +85,16 @@ resource "aws_lb_target_group" "flask_targets" {
   }
 }
 
-resource "aws_lb_target_group_attachment" "flask_attach" {
-  count             = 2
-  target_group_arn  = aws_lb_target_group.flask_targets.arn
-  target_id         = aws_instance.flask_ec2[count.index].id
-  port              = 5000
+resource "aws_lb_target_group_attachment" "flask_attach_public" {
+  target_group_arn = aws_lb_target_group.flask_targets.arn
+  target_id        = data.aws_instance.public_ec2.id
+  port             = 5000
+}
+
+resource "aws_lb_target_group_attachment" "flask_attach_private" {
+  target_group_arn = aws_lb_target_group.flask_targets.arn
+  target_id        = data.aws_instance.private_ec2.id
+  port             = 5000
 }
 
 resource "aws_lb_listener" "http_listener" {
